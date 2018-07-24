61e2ec861628fb5d258ae219b944bc694a302d92
Latlon Technologies <latlonios123@gmail.com>
2018-07-24 09:26:59 +0530

    Initial Commit

---
+++ NewTaxiDriver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTaxiDriver
+//
+//  Created by Admin on 24/07/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewTaxiDriver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTaxiDriver
+//
+//  Created by Admin on 24/07/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
